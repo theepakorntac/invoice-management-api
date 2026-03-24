367530bf546fae04abf8f356f01bfac5accd05f4
theepakorn <theep@KONG>
2026-03-25 00:53:25 +0700

    DTO and Controller init

---
+++ DTOs/RegionDTOs.cs
@@ -0,0 +1,12 @@
+﻿namespace invoice_management_api.DTOs;
+
+public class RegionResponseDTO
+{
+    public int RegionID { get; set; }
+    public string RegionName { get; set; } = string.Empty;
+}
+
+public class RegionCreateDTO
+{
+    public string RegionName { get; set; } = string.Empty;
+}
